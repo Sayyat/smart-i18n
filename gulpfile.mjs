ebc479cff5bf6d21ab6cec5e8fb8f206c1916337
Sayat <sayatraykulov@gmail.com>
2025-09-28 08:05:13 +0500

    feat: add clean task, help text

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,4 +1,5 @@
 export async function registerTasks(gulp) {
+    const clean = (await import("./tasks/clean.js")).default;
     const generateNamespaces = (await import("./tasks/generate-namespaces.js")).default;
     const generateTemplates = (await import("./tasks/generate-templates.js")).default;
     const generateTranslations = (await import("./tasks/generate-translations.js")).default;
@@ -7,6 +8,7 @@ export async function registerTasks(gulp) {
     const initTask = (await import("./tasks/init.js")).default;
     const watchTask = (await import("./tasks/watch.js")).default;
 
+    clean(gulp);
     generateNamespaces(gulp);
     generateTemplates(gulp);
     generateTranslations(gulp);
